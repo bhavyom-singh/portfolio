7fa8503e4883b94914d7068dd5df4023340d3ff8
bhavyom <bhavyom19@gmail.com>
2024-06-17 11:47:14 -0700

    updated files to deploy on vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://bhavyom-singh.github.io/portfolio/",
+  plugins: [react()]
+  
 });
